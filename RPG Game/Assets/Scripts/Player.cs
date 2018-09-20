37371f6471a75e0c61ea0fc40e7f181d29b25cf7
Manuel Ramallo Diaz <manuelramallo@MacBook-Pro-de-Manuel.local>
2018-09-20 21:55:31 +0200

    se comienzaa el juego de RPG 2D estilo Zelda, se pone mapa, personaje, animaciones y script del jugador

---
+++ RPG Game/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed =  4f;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 movimiento = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            0);
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            transform.position + movimiento,
+            speed * Time.deltaTime);
+
+        anim.SetFloat("MoveX", movimiento.x);
+        anim.SetFloat("MoveY", movimiento.y);
+
+
+	}
+}
